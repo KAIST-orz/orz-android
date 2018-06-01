542ff5ddadc5d5dac14ed1098eaa02fb6d8cbf75
hjyoung001 <hjyoung001@gmail.com>
2018-06-02 01:00:55 +0900

    test commit - register account screen switch

--- app/src/main/java/kr/ac/kaist/orz/LoginActivity.java
+++ app/src/main/java/kr/ac/kaist/orz/LoginActivity.java
@@ -1,7 +1,9 @@
 package kr.ac.kaist.orz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void signIn(View v) {
+        //Intent intent = new Intent(getApplicationContext(), );
+        //startActivity(intent);
+    }
+
+    public void registerAccount(View v) {
+        Intent intent = new Intent(getApplicationContext(), RegisterAccountActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/kr/ac/kaist/orz/RegisterAccountActivity.java
+++ app/src/main/java/kr/ac/kaist/orz/RegisterAccountActivity.java
@@ -1,7 +1,9 @@
 package kr.ac.kaist.orz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterAccountActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class RegisterAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_account);
+    }
+
+    public void register(View v) {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
     }
 }
