dc48c0e907628945ec1cdc9551bc0f57aa619498
mhsong95 <tintin5108@gmail.com>
2018-06-01 18:51:53 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kaist/orz/NotificationSettingsActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.kaist.orz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotificationSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_settings);
+    }
+}
---
+++ app/src/main/java/kr/ac/kaist/orz/OpenCoursesActivity.java
@@ -0,0 +1,13 @@
+package kr.ac.kaist.orz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenCoursesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_open_courses);
+    }
+}
