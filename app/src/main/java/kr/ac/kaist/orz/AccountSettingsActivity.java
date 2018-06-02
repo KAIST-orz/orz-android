e9944bdd952dd0b4935b0a80b79af947cb467e76
hjyoung001 <hjyoung001@gmail.com>
2018-06-02 22:35:29 +0900

    implement 1 - sign in, sign up, account setting

--- app/src/main/java/kr/ac/kaist/orz/AccountSettingsActivity.java
+++ app/src/main/java/kr/ac/kaist/orz/AccountSettingsActivity.java
@@ -1,7 +1,13 @@
 package kr.ac.kaist.orz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AccountSettingsActivity extends AppCompatActivity {
 
@@ -10,5 +16,85 @@ public class AccountSettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_settings);
 
+        EditText idView = findViewById(R.id.editText_id);
+        EditText emailView = findViewById(R.id.editText_email);
+        Spinner schoolsSpinner = findViewById(R.id.spinner_schools);
+        CheckBox lecturerCheckBox = findViewById(R.id.checkBox_lecturer);
+
+        idView.setText(getUserID());
+        emailView.setText(getUserEmail());
+        schoolsSpinner.setSelection(getUserSchool());
+        lecturerCheckBox.setChecked(isUserLecturer());
+    }
+
+    public String getUserID() {
+        return "asdf";
+    }
+
+    public String getUserEmail() {
+        return "asdf@asdf";
+    }
+
+    public int getUserSchool() {
+        return 0;
+    }
+
+    public boolean isUserLecturer() {
+        return false;
+    }
+
+    public void update(View v) {
+        EditText idView = findViewById(R.id.editText_id);
+        EditText passView = findViewById(R.id.editText_password);
+        EditText verifyView = findViewById(R.id.editText_verify);
+        EditText emailView = findViewById(R.id.editText_email);
+        Spinner schoolsSpinner = findViewById(R.id.spinner_schools);
+        CheckBox lecturerCheckBox = findViewById(R.id.checkBox_lecturer);
+
+        String id = idView.getText().toString();
+        int pass = passView.getText().toString().hashCode();
+        int verify = verifyView.getText().toString().hashCode();
+        String email = emailView.getText().toString();
+        String school = schoolsSpinner.getSelectedItem().toString();
+        boolean isLecturer = lecturerCheckBox.isChecked();
+
+        if(id.length() == 0)
+            Toast.makeText(this, "The ID is empty", Toast.LENGTH_LONG).show();
+
+        else if(pass == 0)
+            Toast.makeText(this, "The password is empty", Toast.LENGTH_LONG).show();
+
+        else if(verify == 0)
+            Toast.makeText(this, "The password is empty", Toast.LENGTH_LONG).show();
+
+        else if(email.length() == 0)
+            Toast.makeText(this, "The email is empty", Toast.LENGTH_LONG).show();
+
+        else if(!checkID(id))
+            Toast.makeText(this, "The ID already exists", Toast.LENGTH_LONG).show();
+
+        else if(!checkPassword(pass, verify))
+            Toast.makeText(this, "The passwords does not match", Toast.LENGTH_LONG).show();
+
+        else if(!checkEmail(email))
+            Toast.makeText(this, "Inappropriate email format", Toast.LENGTH_LONG).show();
+
+        else {
+            Toast.makeText(this, "update success", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public boolean checkID(String id) {
+        return !id.equals("asdf");
+    }
+
+    public boolean checkPassword(int pass, int verify) {
+        return pass == verify;
+    }
+
+    public boolean checkEmail(String email) {
+        return email.contains("@");
     }
 }
--- app/src/main/java/kr/ac/kaist/orz/LoginActivity.java
+++ app/src/main/java/kr/ac/kaist/orz/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,8 +16,32 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void signIn(View v) {
-        //Intent intent = new Intent(getApplicationContext(), );
-        //startActivity(intent);
+        EditText idView = findViewById(R.id.editText_id);
+        EditText passView = findViewById(R.id.editText_password);
+
+        String id = idView.getText().toString();
+        int pass = passView.getText().toString().hashCode();
+
+        if(checkLogin(id, pass)) {
+            Toast.makeText(this, "signin success", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(getApplicationContext(), OrzMainActivity.class);
+            startActivity(intent);
+        }
+        else
+            Toast.makeText(this, "signin failed", Toast.LENGTH_LONG).show();
+    }
+
+    public boolean checkLogin(String id, int pass) {
+        if(pass == getPassword(id))
+            return true;
+        return false;
+    }
+
+    public int getPassword(String id) {
+        if(id.equals("asdf"))
+            return "asdf".hashCode();
+        else
+            return -1;
     }
 
     public void registerAccount(View v) {
