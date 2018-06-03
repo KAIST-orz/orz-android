e23cb74ef18574e5e82cf77bdf9e85c8d81001aa
hjyoung001 <hjyoung001@gmail.com>
2018-06-03 20:57:12 +0900

    merge commit

--- app/src/main/java/kr/ac/kaist/orz/AssignmentTabFragment.java
+++ app/src/main/java/kr/ac/kaist/orz/AssignmentTabFragment.java
@@ -96,20 +96,20 @@ public class AssignmentTabFragment extends Fragment {
             mListener.onFragmentInteraction(uri);
         }
     }
-/*
-    @Override
-<<<<<<< .merge_file_a19328
-=======
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    /*
+        @Override
+    <<<<<<< .merge_file_a19328
+    =======
+        public void onAttach(Context context) {
+            super.onAttach(context);
+            if (context instanceof OnFragmentInteractionListener) {
+                mListener = (OnFragmentInteractionListener) context;
+            } else {
+                throw new RuntimeException(context.toString()
+                        + " must implement OnFragmentInteractionListener");
+            }
         }
-    }
-*/
+    */
     @Override
 
     public void onDetach() {
