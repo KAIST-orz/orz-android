91d013fc7c34f67f9e934dd04b0bc68666ae2075
hy2850 <hy2850@naver.com>
2018-06-06 01:10:03 +0900

    리스트에 데이터를 추가하는 부분을 별도의 method 2개(onResume, addData)로 분리.

--- app/src/main/java/kr/ac/kaist/orz/MyCoursesActivity.java
+++ app/src/main/java/kr/ac/kaist/orz/MyCoursesActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -17,29 +18,24 @@ public class MyCoursesActivity extends AppCompatActivity {
     private ListView m_ListView;
     private ListAdapter m_Adapter;
 
+    //데이터를 저장하게 되는 리스트
+    private List<myCourseInformation> list = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_courses);
 
-        /*
-        //데이터를 저장하게 되는 리스트
-        final List<myCourseInformation> list = new ArrayList<>();
-        //리스트뷰에 보여질 아이템을 추가
-        list.add(new myCourseInformation("Logical Writing", "HSS001(K)","Jaeun Oh"));
-        list.add(new myCourseInformation("Interactive Product Design", "ID301(A)","Woohun Lee"));
-        list.add(new myCourseInformation("Computer Organization", "CS311","Hyunsoo Yoon"));
-        list.add(new myCourseInformation("Introduction to Software Engineering", "CS350","Doo-Hwan Bae"));
+        //List에 정보 추가 (이 정보들이 listView로 보여짐)
+        //addData();
 
         // Xml에서 추가한 ListView 연결
-        m_ListView = (ListView)findViewById(R.id.listview);
-
+        m_ListView = (ListView)findViewById(R.id.listview_myCourses);
         //리스트뷰와 리스트를 연결하기 위해 사용되는 어댑터
         m_Adapter = new myCourseViewAdapter(this, list);
-
         //리스트뷰의 어댑터를 지정해준다.
         m_ListView.setAdapter(m_Adapter);
-        */
+
 
         //Floating Action Button을 누르면 OpenCourse 페이지로 넘어갈 수 있게.
         FloatingActionButton fab = findViewById(R.id.myCourse_fab);
@@ -53,44 +49,28 @@ public class MyCoursesActivity extends AppCompatActivity {
                 startActivity(intent); //다음 화면으로 넘어감
             }
         });
-
-        /*
-        ///////////////////////////////////////////////////////////////////////////////
-        //Testing
-        //리스트뷰의 아이템을 클릭시 해당 아이템의 문자열을 가져오기 위한 처리
-        m_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-                //--For testing : 버튼 클릭 시 assignment 이름 출력--
-//                Toast.makeText(getApplicationContext() , "Test successful!!!",Toast.LENGTH_LONG).show();
-                //
-
-                // 상세정보 화면으로 이동하기(인텐트 날리기)
-                // 1. 다음화면을 만든다
-                // 2. AndroidManifest.xml 에 화면을 등록한다
-                // 3. Intent 객체를 생성하여 날린다
-                Intent intent = new Intent(
-                        getApplicationContext(), // 현재화면의 제어권자
-                        AssignmentDetailsActivity.class); // 다음넘어갈 화면
-
-                // intent 객체에 데이터를 실어서 보내기
-                // 리스트뷰 클릭시 인텐트 (Intent) 생성하고 position 값을 이용하여 인텐트로 넘길값들을 넘긴다
-                intent.putExtra("Assignment ID", list.get(position).getCourseName());
-
-                startActivity(intent);
-
-            }
-        });
-        ///////////////////////////////////////////////////////////////////////////////
-        */
-
-        /*
-        m_Adapter.add(new myCourseInformation("Logical Writing", "HSS001(K)","Jaeun Oh"));
-        m_Adapter.add(new myCourseInformation("Interactive Product Design", "ID301(A)","Woohun Lee"));
-        m_Adapter.add(new myCourseInformation("Computer Organization", "CS311","Hyunsoo Yoon"));
-        m_Adapter.add(new myCourseInformation("Introduction to Software Engineering", "CS350","Doo-Hwan Bae"));
-        */
     }
+
+    protected void onResume()
+    {
+        super.onResume();
+
+        // List 초기화
+        list.clear();
+
+        // List에 데이터 입력
+        addData();
+    }
+
+
+    //리스트뷰에 보여질 아이템을 list에 추가
+    //DB에서 전달받은 값을 list에 add하면 될 듯.
+    private void addData()
+    {
+        list.add(new myCourseInformation("Logical Writing", "HSS001(K)","Jaeun Oh", 100));
+        list.add(new myCourseInformation("Interactive Product Design", "ID301(A)","Woohun Lee", 200));
+        list.add(new myCourseInformation("Computer Organization", "CS311","Hyunsoo Yoon", 301));
+        list.add(new myCourseInformation("Introduction to Software Engineering", "CS350","Doo-Hwan Bae", 404));
+    }
+
 }
