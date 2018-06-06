39bdc917c52bd2f5eff2f150a6c4cda51f725fc9
hjyoung001 <hjyoung001@gmail.com>
2018-06-06 22:22:15 +0900

    implemented schedule detail, register schedule, register assignment, register lecture, lecture details

--- app/src/main/java/kr/ac/kaist/orz/RegisterScheduleActivity.java
+++ app/src/main/java/kr/ac/kaist/orz/RegisterScheduleActivity.java
@@ -1,7 +1,15 @@
 package kr.ac.kaist.orz;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class RegisterScheduleActivity extends AppCompatActivity {
 
@@ -9,5 +17,52 @@ public class RegisterScheduleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_schedule);
+    }
+
+    public void selectTime(final View v) {
+        final AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        final DatePicker dp = new DatePicker(this);
+        final TimePicker tp = new TimePicker(this);
+
+        adb.setView(dp);
+        adb.setPositiveButton("set",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Toast.makeText(getApplicationContext(), "test1", Toast.LENGTH_LONG).show();
+                        adb.setPositiveButton("set",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        //Toast.makeText(getApplicationContext(), "test2", Toast.LENGTH_LONG).show();
+                                        String s = String.valueOf(dp.getYear()) + "-" + String.valueOf(dp.getMonth() + 1) + "-" + String.valueOf(dp.getDayOfMonth()) + " " + String.valueOf(tp.getCurrentHour()) + ":" + String.valueOf(tp.getCurrentMinute());
+                                        ((Button)v).setText(s);
+                                        Toast.makeText(getApplicationContext(), String.valueOf(dp.getYear()) + "-" + String.valueOf(dp.getMonth() + 1) + "-" + String.valueOf(dp.getDayOfMonth()) + "\n" + String.valueOf(tp.getCurrentHour()) + ":" + String.valueOf(tp.getCurrentMinute()), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                        adb.setNegativeButton("cancel", null);
+                        adb.setView(tp);
+                        adb.show();
+                    }
+                });
+        adb.setNegativeButton("cancel", null);
+        adb.show();
+    }
+
+    public void create(View v) {
+        EditText schedule_name = findViewById(R.id.editText_schedule_name);
+        Button start_time = findViewById(R.id.button_start_time);
+        Button end_time = findViewById(R.id.button_end_time);
+
+        if(schedule_name.length() == 0)
+            Toast.makeText(this, "schedule name can not be empty", Toast.LENGTH_LONG).show();
+        else if(start_time.getText().equals("start time"))
+            Toast.makeText(this, "start time can not be empty", Toast.LENGTH_LONG).show();
+        else if(end_time.getText().equals("end time"))
+            Toast.makeText(this, "end time can not be empty", Toast.LENGTH_LONG).show();
+        else {
+            Toast.makeText(this, "schedule created", Toast.LENGTH_LONG).show();
+            finish();
+        }
     }
 }
