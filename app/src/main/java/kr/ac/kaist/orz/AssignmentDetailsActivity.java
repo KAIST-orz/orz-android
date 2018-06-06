29cdd6dad9232d173bc68926bbe72247010953c6
zealotzealot <hsh0908y@gmail.com>
2018-06-07 00:24:33 +0900

    Revert "Implement sign in, sign up, loading student's courses"

--- app/src/main/java/kr/ac/kaist/orz/AssignmentDetailsActivity.java
+++ app/src/main/java/kr/ac/kaist/orz/AssignmentDetailsActivity.java
@@ -1,13 +1,240 @@
 package kr.ac.kaist.orz;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AssignmentDetailsActivity extends AppCompatActivity {
+
+    List<String> notification_time = new ArrayList<String>();
+    List<String> time_for_assignment = new ArrayList<String>();
+
+    static int e_time = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_assignment_details);
+
+        notification_time.add("5 minute before (Default)");
+        notification_time.add("10 minute before");
+        notification_time.add("Add more notification");
+
+        time_for_assignment.add("7:00PM ~ 11:00PM, April 5th");
+        time_for_assignment.add("Add more time");
+
+        final AlertDialog.Builder adb = new AlertDialog.Builder(this);
+
+        ListView listview1 = findViewById(R.id.listView_notification);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, notification_time);
+        listview1.setAdapter(adapter);
+        listview1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                if(notification_time.get(position).equals("Add more notification")) {
+                    selectAlarm();
+                }
+                else {
+                    adb.setTitle("remove notification?");
+                    adb.setPositiveButton("Yes",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Toast.makeText(getApplicationContext(), "remove alarm #" + String.valueOf(position), Toast.LENGTH_LONG).show();
+                                }
+                            });
+                    adb.setNegativeButton("No", null);
+                    adb.show();
+                }
+            }
+        });
+
+        ListView listview2 = findViewById(R.id.listView_time_for_assignment);
+        adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, time_for_assignment);
+        listview2.setAdapter(adapter);
+        listview2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(time_for_assignment.get(position).equals("Add more time")) {
+                    selectDuration();
+                }
+                else {
+                    Intent intent = new Intent(getApplicationContext(), ScheduleDetailsActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        setListViewHeightBasedOnChildren(listview1);
+        setListViewHeightBasedOnChildren(listview2);
+
+        TextView subject_assignment = findViewById(R.id.textView_subject_assignment);
+        subject_assignment.setText("Computer Architecture\nHomework 4");
+
+        Button due = findViewById(R.id.button_due);
+        due.setText("11:59 PM, April 5th");
+        due.setEnabled(false);
+
+        Button estimated_time = findViewById(R.id.button_estimated_time);
+        estimated_time.setText(String.valueOf(e_time) .concat(" hours\nOther students estimated 4.6 hours"));
+
+        TextView description = findViewById(R.id.textView_description);
+        description.setText("P. 3-11, 3-17, 3-19, 3-20, 3-21");
+    }
+
+    public void selectAlarm() {
+        final String[] times = new String[] {"1min", "3min", "5min", "10min", "30min", "1hour", "2hour", "3hour"};
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        adb.setTitle("set time");
+        adb.setSingleChoiceItems(times, 1, null);
+        adb.setPositiveButton("close",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ListView lw = ((AlertDialog)dialog).getListView();
+                        Object checkedItem = lw.getAdapter().getItem(lw.getCheckedItemPosition());
+                        Toast.makeText(getApplicationContext(), "time set to ".concat(checkedItem.toString()), Toast.LENGTH_LONG).show();
+                    }
+                });
+        adb.show();
+    }
+
+    public void selectDuration() {
+        final AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        final DatePicker dp1 = new DatePicker(this);
+        final TimePicker tp1 = new TimePicker(this);
+        final DatePicker dp2 = new DatePicker(this);
+        final TimePicker tp2 = new TimePicker(this);
+
+        adb.setPositiveButton("set",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Toast.makeText(getApplicationContext(), "test1", Toast.LENGTH_LONG).show();
+                        adb.setPositiveButton("set",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        //Toast.makeText(getApplicationContext(), "test2", Toast.LENGTH_LONG).show();
+                                        adb.setPositiveButton("set",
+                                                new DialogInterface.OnClickListener() {
+                                                    @Override
+                                                    public void onClick(DialogInterface dialog, int which) {
+                                                        //Toast.makeText(getApplicationContext(), "test3", Toast.LENGTH_LONG).show();
+                                                        adb.setPositiveButton("set",
+                                                                new DialogInterface.OnClickListener() {
+                                                                    @Override
+                                                                    public void onClick(DialogInterface dialog, int which) {
+                                                                        //Toast.makeText(getApplicationContext(), "test4", Toast.LENGTH_LONG).show();
+                                                                        Toast.makeText(getApplicationContext(), String.valueOf(dp1.getYear()) + "-" + String.valueOf(dp1.getMonth() + 1) + "-" + String.valueOf(dp1.getDayOfMonth()) + "\n" + String.valueOf(tp1.getCurrentHour()) + ":" + String.valueOf(tp1.getCurrentMinute()) + "\n" + String.valueOf(dp2.getYear()) + "-" + String.valueOf(dp2.getMonth() + 1) + "-" + String.valueOf(dp2.getDayOfMonth()) + "\n" + String.valueOf(tp2.getCurrentHour()) + ":" + String.valueOf(tp2.getCurrentMinute()), Toast.LENGTH_LONG).show();
+                                                                    }
+                                                                });
+                                                        adb.setNegativeButton("cancel", null);
+                                                        adb.setView(tp2);
+                                                        adb.show();
+                                                    }
+                                                });
+                                        adb.setNegativeButton("cancel", null);
+                                        adb.setView(dp2);
+                                        adb.show();
+                                    }
+                                });
+                        adb.setNegativeButton("cancel", null);
+                        adb.setView(tp1);
+                        adb.show();
+                    }
+                });
+        adb.setNegativeButton("cancel", null);
+        adb.setView(dp1);
+        adb.show();
+    }
+
+    public void selectTime() {
+        final AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        final DatePicker dp = new DatePicker(this);
+        final TimePicker tp = new TimePicker(this);
+
+        adb.setView(dp);
+        adb.setPositiveButton("set",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Toast.makeText(getApplicationContext(), "test1", Toast.LENGTH_LONG).show();
+                        adb.setPositiveButton("set",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        //Toast.makeText(getApplicationContext(), "test2", Toast.LENGTH_LONG).show();
+                                        Toast.makeText(getApplicationContext(), String.valueOf(dp.getYear()) + "-" + String.valueOf(dp.getMonth() + 1) + "-" + String.valueOf(dp.getDayOfMonth()) + "\n" + String.valueOf(tp.getCurrentHour()) + ":" + String.valueOf(tp.getCurrentMinute()), Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                        adb.setNegativeButton("cancel", null);
+                        adb.setView(tp);
+                        adb.show();
+                    }
+                });
+        adb.setNegativeButton("cancel", null);
+        adb.show();
+    }
+
+    public void enter_expected_time(View v) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("enter expected time");
+
+        final EditText time = new EditText(this);
+        time.setInputType(InputType.TYPE_CLASS_NUMBER);
+        alert.setView(time);
+
+        alert.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "time set to ".concat(time.getText().toString()).concat(String.valueOf(e_time)), Toast.LENGTH_LONG).show();
+                e_time = Integer.parseInt(time.getText().toString());
+                finish();
+                startActivity(getIntent());
+            }
+        });
+
+        alert.setNegativeButton("cancel", null);
+
+        alert.show();
+    }
+
+    public static void setListViewHeightBasedOnChildren(ListView listView) {
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            // pre-condition
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0; i < listAdapter.getCount(); i++) {
+            View listItem = listAdapter.getView(i, null, listView);
+            listItem.measure(0, 0);
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight + (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        listView.setLayoutParams(params);
+        listView.requestLayout();
     }
 }
--- app/src/main/java/kr/ac/kaist/orz/AssignmentTabFragment.java
+++ app/src/main/java/kr/ac/kaist/orz/AssignmentTabFragment.java
@@ -1,12 +1,21 @@
 package kr.ac.kaist.orz;
 
-import android.content.Intent;
-import android.net.Uri;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
+
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -21,25 +30,28 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link AssignmentTabFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link AssignmentTabFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class AssignmentTabFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class AssignmentTabFragment extends Fragment implements DialogInterface.OnClickListener {
+    private ListView assignmentListView;    // The ListView to display items on.
+    private AssignmentListViewAdapter listViewAdapter;      // The listViewAdapter that provides data to the ListView.
+    private ArrayList<Assignment> assignmentList;   // The list of assignment data.
 
-    private OnFragmentInteractionListener mListener;
+    // The item names to show on the dialog for picking sorting criteria.
+    private static final String SORT_DUE = "Due";
+    private static final String SORT_COURSE = "Course";
+    private static final String SORT_SIGNIFICANCE = "Significance";
+    private static final String SORT_ESTIMATE = "Average time estimate";
+
+    // Current sorting criteria. Initially sort by due date.
+    private String sortingCriteria = SORT_DUE;
 
     ListView m_ListView;
-    assignmentTabViewAdapter m_Adapter;
+    AssignmentListViewAdapter m_Adapter;
 
     public AssignmentTabFragment() {
         // Required empty public constructor
@@ -55,10 +67,6 @@ public class AssignmentTabFragment extends Fragment {
     public static AssignmentTabFragment newInstance() {
         AssignmentTabFragment fragment = new AssignmentTabFragment();
         Bundle args = new Bundle();
-        /*
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        */
         fragment.setArguments(args);
         return fragment;
     }
@@ -67,20 +75,28 @@ public class AssignmentTabFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        }
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate layout for this fragment.
         View view = inflater.inflate(R.layout.fragment_assignment_tab, container, false);
+        assignmentListView = (ListView) view.findViewById(R.id.listview_assignment);
 
-        int assignment_id=123; //각 assignment 마다 unique 하게 부여되며, assignment들을 구별하고 파악하는데 사용됨.
-                           //DB에서 값을 받아서, AssignmentTabFragment의 버튼 클릭시, Intent를 통해 'AssignmentDetailsActivity.java'로 전달될 것임.
+        // TODO: get the list of assignments from server.
+        // listViewAdapter.notifyDataSetChanged();
 
+        // Set adapter to the ListView.
+        // assignmentListView.setAdapter(listViewAdapter);
 
         //데이터를 저장하게 되는 리스트
         final List<Assignment> list = new ArrayList<>();
+
+        m_Adapter = new AssignmentListViewAdapter(getContext(), list);
+        assignmentListView.setAdapter(m_Adapter);
+
         //리스트뷰에 보여질 아이템을 추가
         OrzApi api = ApplicationController.getInstance().getApi();
         User user = ApplicationController.getInstance().getUser();
@@ -101,89 +117,65 @@ public class AssignmentTabFragment extends Fragment {
             }
         });
 
-        // Xml에서 추가한 ListView 연결
-        m_ListView = (ListView)view.findViewById(R.id.listview_assignment);
-
-        //리스트뷰와 리스트를 연결하기 위해 사용되는 어댑터
-        m_Adapter = new assignmentTabViewAdapter(getActivity(), list);
-        //리스트뷰의 어댑터를 지정해준다.
-        m_ListView.setAdapter(m_Adapter);
-
-
-        //AssignmentTabFragment의 listview에 보여지는 row를 클릭 할 경우, AssignmentDetailsActivity로 넘어가는 기능
-        //[Ref]http://bitsoul.tistory.com/95 [Happy Programmer~]
-        m_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-                //--For testing : 버튼 클릭 시 assignment 이름 출력--
-                //Toast.makeText(getActivity() , list.get(position).getName(),Toast.LENGTH_LONG).show();
-
-                // 상세정보 화면으로 이동하기(인텐트 날리기)
-                // 1. 다음화면을 만든다 (생략)
-                // 2. AndroidManifest.xml 에 화면을 등록한다 (생략)
-                // 3. Intent 객체를 생성하여 날린다
-                Intent intent = new Intent(
-                        getActivity(), // 현재화면의 제어권자 (fragment의 경우 getActivity, activity의 경우 getApplicationContext 함수 사용)
-                        AssignmentDetailsActivity.class); // 다음넘어갈 화면
-
-                // intent 객체에 데이터를 실어서 보내기
-                // 리스트뷰 클릭시 인텐트 (Intent) 생성하고 position 값을 이용하여 인텐트로 넘길값들을 넘긴다
-                // 데이터 주고 받는 법 : http://pds0819.tistory.com/entry/%EC%9D%B8%ED%85%90%ED%8A%B8Intent%EB%A1%9C-%EB%8D%B0%EC%9D%B4%ED%84%B0-%EC%A0%84%EB%8B%ACputExtra-getExtras-%EA%B7%B8%EB%A6%AC%EA%B3%A0-TapHost%EC%97%90%EC%84%9C-%EB%8B%A4%EB%A5%B8-%EC%97%91%ED%8B%B0%EB%B9%84%ED%8B%B0%EB%A1%9C-%EB%B3%80%EC%88%98%EA%B0%92-%EC%A0%84%EB%8B%AC
-                intent.putExtra("Assignment ID", list.get(position).getID());
-
-                startActivity(intent);
-            }
-        });
-
-        // Inflate the layout for this fragment
         return view;
     }
 
-/*
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    private void getAssignments() {
+        assignmentList.clear();
+    }
+
+    // Shows a dialog that lets the user pick a sorting criteria.
+    public void showSortingCriteriaDialog(Context context) {
+        final String[] criteria = new String[]{SORT_DUE, SORT_COURSE, SORT_SIGNIFICANCE, SORT_ESTIMATE};
+        AlertDialog.Builder adb = new AlertDialog.Builder(context);
+        adb.setTitle("Set sorting criteria");
+
+        // Show the current sorting criteria as checked.
+        int checkedItem;
+        for (checkedItem = 0; checkedItem < criteria.length; checkedItem++) {
+            if (sortingCriteria.equals(criteria[checkedItem])) {
+                break;
+            }
         }
+
+        adb.setSingleChoiceItems(criteria, checkedItem, null);
+        adb.setPositiveButton("close", this);
+        adb.show();
     }
 
-    /*
     @Override
-<<<<<<< .merge_file_a19328
-=======
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void onClick(DialogInterface dialogInterface, int i) {
+        ListView listView = ((AlertDialog) dialogInterface).getListView();
+        Object checkedItem = listView.getAdapter().getItem(listView.getCheckedItemPosition());
+
+        // The comparator to sort assignmentList with according to the picked one.
+        Comparator<Assignment> assignmentComparator;
+        // Set current sorting criteria.
+        sortingCriteria = checkedItem.toString();
+
+        /*
+        switch (sortingCriteria) {
+            case SORT_DUE:
+                assignmentComparator = Assignment.DUE_COMPARATOR;
+                break;
+            case SORT_COURSE:
+                assignmentComparator = Assignment.COURSE_COMPARATOR;
+                break;
+            case SORT_SIGNIFICANCE:
+                assignmentComparator = Assignment.SIGNIFICANCE_COMPARATOR;
+                break;
+            case SORT_ESTIMATE:
+                assignmentComparator = Assignment.ESTIMATE_COMPARATOR;
+                break;
+            default:
+                assignmentComparator = null;
         }
+
+        // Sort the assignmentList with picked sorting criteria, then notify assignmentListViewAdapter.
+        if (assignmentComparator != null) {
+            Collections.sort(assignmentList, assignmentComparator);
+            listViewAdapter.notifyDataSetChanged();
+        }
+        */
     }
-*/
-    @Override
-
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
-
 }
