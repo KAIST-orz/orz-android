835095fc24e352d60eab81f97e747db571c2d687
zealotzealot <hsh0908y@gmail.com>
2018-06-08 07:03:53 +0900

    Prevent end time being before start time in registering schedule

--- app/src/main/java/kr/ac/kaist/orz/RegisterScheduleActivity.java
+++ app/src/main/java/kr/ac/kaist/orz/RegisterScheduleActivity.java
@@ -232,6 +232,9 @@ public class RegisterScheduleActivity extends AppCompatActivity {
         else if(end_time.getText().equals("End Time")) {
             Toast.makeText(this, "End time can not be empty", Toast.LENGTH_LONG).show();
         }
+        else if(end_time.getText().toString().compareTo(start_time.getText().toString()) < 0) {
+            Toast.makeText(this, "End time should be after start time", Toast.LENGTH_LONG).show();
+        }
         else {
             // Post the new schedule.
             OrzApi api = ApplicationController.getInstance().getApi();
