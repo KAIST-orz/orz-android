5220fb5d3bfed5aa5aacaefa72b199d8e90395b6
mhsong95 <tintin5108@gmail.com>
2018-06-08 08:12:03 +0900

    Made a lecturer be able to see account settings.

--- app/src/main/java/kr/ac/kaist/orz/LecturerCoursesActivity.java
+++ app/src/main/java/kr/ac/kaist/orz/LecturerCoursesActivity.java
@@ -8,6 +8,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
@@ -142,5 +144,30 @@ public class LecturerCoursesActivity extends AppCompatActivity {
     public void refreshView(){
         finish();
         startActivity(getIntent());
+    }
+
+    // Let the lecturer see the account settings page.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_orz_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, AccountSettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
